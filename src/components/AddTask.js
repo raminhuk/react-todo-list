218329474e908216a159fd84289e8f23e660452e
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2022-02-27 22:46:25 -0300

    Criada as primeiras funcionalidades do projeto, Adicionar e excluir tasks

---
+++ src/components/AddTask.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { Button, HStack, Input, useToast } from "@chakra-ui/react";
+import { nanoid } from 'nanoid';
+
+function AddTask({ addTask }) {
+    const toast = useToast();
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        if (!content) {
+            toast({
+                title: 'Sem conteúdo',
+                status: 'warning',
+                duration: 2000,
+                isClosable: true,
+            });
+            return;
+        }
+        
+        const task = {
+            id: nanoid(),
+            body: content,
+        };
+        
+        addTask(task);
+        setContent('');
+    }
+
+    const [content, setContent] = useState('');
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <HStack mt='4' mb='4'>
+                <Input
+                    h='46'
+                    variant='filled'
+                    placeholder='Digite sua tarefa'
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                />
+                <Button
+                colorScheme='blue'
+                px='8'
+                pl='10'
+                pr='10'
+                h='46' 
+                type='submit'>Adicionar</Button>
+            </HStack>
+        </form>
+    );
+}
+
+export default AddTask;
