d4d9f36b5f90bc1200e21642b48aac1d5e48d1ec
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2022-03-21 21:20:24 -0300

    Add function confirm delete

---
+++ src/components/DeleteTask.js
@@ -0,0 +1,87 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Text,
+    useDisclosure,
+    IconButton
+  } from '@chakra-ui/react'
+  import { useState } from 'react';
+  import React from 'react';
+  import { FiTrash2 } from 'react-icons/fi'
+
+  
+
+  function DeleteAllTask({ deleteTaskAll }) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <>
+            <Button
+                colorScheme='gray'
+                px='8'
+                h='45'
+                color='gray.500'
+                mt='8'
+                onClick={onOpen}
+                >
+                Excluir Todos
+            </Button>
+
+            <Modal isCentered isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent w='90%'>
+                <ModalHeader>
+                    Você realmente deseja excluir a todas as tarefa?
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalFooter>
+                <Button mr={3} onClick={onClose}>Não</Button>
+                <Button colorScheme='blue' onClick={() => deleteTaskAll()}>
+                    Sim
+                </Button>
+                </ModalFooter>
+            </ModalContent>
+            </Modal>
+        </>
+    )
+  }
+
+  function DeleteTask({ task, deleteTask}) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    return (
+      <>
+        <IconButton
+            icon={<FiTrash2 />}
+            isRound='true'
+            onClick={onOpen}
+        />
+
+        <Modal isCentered isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent w='90%'>
+            <ModalHeader>
+                Você realmente deseja excluir a tarefa:
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                <Text>{task.body} ?</Text>
+            </ModalBody>
+            <ModalFooter>
+              <Button mr={3} onClick={onClose}>Não</Button>
+              <Button colorScheme='blue' onClick={() => deleteTask(task.id, onClose)}>
+                Sim
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
+
+export { DeleteTask, DeleteAllTask }
